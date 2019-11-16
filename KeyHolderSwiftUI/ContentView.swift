423698b22ecf8f6663c8c6482d0598ab0f573564
Giovane Menezes <giovane.menezes@live.com>
2019-11-16 12:28:39 -0300

    First Commit

---
+++ KeyHolderSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeyHolderSwiftUI
+//
+//  Created by Giovane Silva de Menezes Cavalcante on 16/11/19.
+//  Copyright © 2019 GSMenezes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
